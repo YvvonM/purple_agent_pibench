b5572ab287aded57c6f958aaede61ca2c52a53ea
Yvvon Majala <149659748+YvvonM@users.noreply.github.com>
2026-05-29 02:29:21 -0700

    chunking the docs

---
+++ chunking_docs.py
@@ -0,0 +1,5 @@
+### document for chuncking the json files
+from json
+from langchain.schema import Document
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
